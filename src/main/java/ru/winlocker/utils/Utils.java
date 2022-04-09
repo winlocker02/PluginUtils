9c6aefd3bf47b2aa98a0b57f929d7bf40523cc08
WinLocker221 <zhenya-kopylov-02@mail.ru>
2022-04-09 16:32:32 +0800

    Initial commit

---
+++ src/main/java/ru/winlocker/utils/Utils.java
@@ -0,0 +1,17 @@
+package ru.winlocker.utils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Utils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
