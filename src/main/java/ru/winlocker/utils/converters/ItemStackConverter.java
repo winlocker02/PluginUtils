d298d187b15ed86957fc429012541f906ebdd40a
WinLocker221 <zhenya-kopylov-02@mail.ru>
2022-05-09 18:59:28 +0800

    Update

--- src/main/java/ru/winlocker/utils/converters/ItemStackConverter.java
+++ src/main/java/ru/winlocker/utils/converters/ItemStackConverter.java
@@ -2,17 +2,22 @@ package ru.winlocker.utils.converters;
 
 import com.cryptomorin.xseries.*;
 import org.bukkit.inventory.*;
+import ru.winlocker.utils.*;
 import ru.winlocker.utils.config.conversion.*;
 import ru.winlocker.utils.config.data.*;
 
 public class ItemStackConverter implements TypeConverter<ItemStack> {
+
+    public static ItemStackConverter create() {
+        return new ItemStackConverter();
+    }
 
     @Override
     public ItemStack loadFrom(DataHolder dataHolder, String path, ItemStack currentValue) {
         if(dataHolder instanceof ConfigurationSectionDataHolder) {
             ConfigurationSectionDataHolder configuration = (ConfigurationSectionDataHolder) dataHolder;
 
-            return XItemStack.deserialize(configuration.getSection().getConfigurationSection(path));
+            return XItemStack.deserialize(configuration.getSection().getConfigurationSection(path), Utils::color);
         }
         return null;
     }
