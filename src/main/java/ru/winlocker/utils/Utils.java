d06daec3fd2ccf32e971a6f4e0a1d54a6b165211
BuildTools <unconfigured@null.spigotmc.org>
2024-03-31 17:19:55 +0800

    Add supports hex colors

--- src/main/java/ru/winlocker/utils/Utils.java
+++ src/main/java/ru/winlocker/utils/Utils.java
@@ -1,19 +1,41 @@
 package ru.winlocker.utils;
 
+import com.cryptomorin.xseries.XMaterial;
 import com.cryptomorin.xseries.messages.*;
+import lombok.NonNull;
 import org.bukkit.*;
 import org.bukkit.command.*;
 import org.bukkit.entity.*;
 
 import java.text.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.*;
 
 public class Utils {
 
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#0.0");
+    private static final Pattern HEX_PATTERN = Pattern.compile("#[a-fA-F0-9]{6}");
 
-    public static String color(final String text) {
+    public static String color(@NonNull String text) {
+        if(XMaterial.supports(16)) {
+            Matcher matcher = HEX_PATTERN.matcher(text);
+
+            while (matcher.find()) {
+                String hexCode = text.substring(matcher.start(), matcher.end());
+                String replaceSharp = hexCode.replace('#', 'x');
+
+                char[] ch = replaceSharp.toCharArray();
+                StringBuilder builder = new StringBuilder();
+                for (char c : ch) {
+                    builder.append("&").append(c);
+                }
+
+                text = text.replace(hexCode, builder.toString());
+                matcher = HEX_PATTERN.matcher(text);
+            }
+        }
         return ChatColor.translateAlternateColorCodes('&', text);
     }
 
